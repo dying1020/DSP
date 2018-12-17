47800ce2097c137da89ee83228a0c06fed4fe66f
陳代穎 <dying1020@gmail.com>
2018-12-17 13:25:02 +0800

    hw1: Transform tab to space

--- hw1/dsp_hw1/c_cpp/myhead.h
+++ hw1/dsp_hw1/c_cpp/myhead.h
@@ -12,14 +12,14 @@
 #endif
 
 typedef struct {
-	int seq_num;
-	int seq[MAX_SEQ];
+    int seq_num;
+    int seq[MAX_SEQ];
 } Observation;
 
 typedef struct {
-	int seq_num;
-	int state_num;
-	double table[MAX_SEQ][MAX_STATE];
+    int seq_num;
+    int state_num;
+    double table[MAX_SEQ][MAX_STATE];
 } Table;
 
 typedef struct {
@@ -35,51 +35,51 @@ typedef struct {
  */
 int get_data(Observation *observs, const char *filename)
 {
-	int i = 0, j, index;
-	FILE *fp = open_or_die(filename, "r");
+    int i = 0, j, index;
+    FILE *fp = open_or_die(filename, "r");
 
-	char token[MAX_LINE] = "";
-	while (fscanf(fp, "%s", token) > 0) {
-		if (token[0] == '\0' || token[0] == '\n') {
-			continue;
-		}
-		
-		for (j = 0; j < MAX_LINE; j++) {
-			switch (token[j]) {
-				case 'A':
-					index = 0;
-					break;
-				case 'B':
-					index = 1;
-					break;
-				case 'C':
-					index = 2;
-					break;
-				case 'D':
-					index = 3;
-					break;
-				case 'E':
-					index = 4;
-					break;
-				case 'F':
-					index = 5;
-					break;
-				case '\0':
-				default:
-					index = -1;
-					break;
-			}
+    char token[MAX_LINE] = "";
+    while (fscanf(fp, "%s", token) > 0) {
+        if (token[0] == '\0' || token[0] == '\n') {
+            continue;
+        }
+        
+        for (j = 0; j < MAX_LINE; j++) {
+            switch (token[j]) {
+                case 'A':
+                    index = 0;
+                    break;
+                case 'B':
+                    index = 1;
+                    break;
+                case 'C':
+                    index = 2;
+                    break;
+                case 'D':
+                    index = 3;
+                    break;
+                case 'E':
+                    index = 4;
+                    break;
+                case 'F':
+                    index = 5;
+                    break;
+                case '\0':
+                default:
+                    index = -1;
+                    break;
+            }
 
-			if (index < 0) {
-				observs[i].seq_num = j;
-				i++;
-				break;
-			}
-			observs[i].seq[j] = index;
-		}
-	}
-	fclose(fp);
-	return i;
+            if (index < 0) {
+                observs[i].seq_num = j;
+                i++;
+                break;
+            }
+            observs[i].seq[j] = index;
+        }
+    }
+    fclose(fp);
+    return i;
 }
 
 #endif
--- hw1/dsp_hw1/c_cpp/train.c
+++ hw1/dsp_hw1/c_cpp/train.c
@@ -10,35 +10,35 @@
  */
 void forward_algo(HMM *hmm, Observation *observ, Table *alpha)
 {
-	int i, j, t;
-	alpha->seq_num = observ->seq_num;
-	alpha->state_num = hmm->state_num;
+    int i, j, t;
+    alpha->seq_num = observ->seq_num;
+    alpha->state_num = hmm->state_num;
 
-	// Initialization
-	for (i = 0; i < hmm->state_num; i++) {
-		alpha->table[0][i] = hmm->initial[i] * hmm->observation[observ->seq[0]][i]; // alpha[0][i] = pi[i] * b[o_1][i]
-	}
+    // Initialization
+    for (i = 0; i < hmm->state_num; i++) {
+        alpha->table[0][i] = hmm->initial[i] * hmm->observation[observ->seq[0]][i]; // alpha[0][i] = pi[i] * b[o_1][i]
+    }
 
-	// Induction
-	for (t = 0; t < observ->seq_num - 1; t++) {
-		for (j = 0; j < hmm->state_num; j++) {
-			double accum = 0;
+    // Induction
+    for (t = 0; t < observ->seq_num - 1; t++) {
+        for (j = 0; j < hmm->state_num; j++) {
+            double accum = 0;
 
-			for (i = 0; i < hmm->state_num; i++) {
-				accum += alpha->table[t][i] * hmm->transition[i][j];
-			}
+            for (i = 0; i < hmm->state_num; i++) {
+                accum += alpha->table[t][i] * hmm->transition[i][j];
+            }
 
-			alpha->table[t+1][j] = accum * hmm->observation[observ->seq[t+1]][j]; // alpha[t][j] = \sum{alpha[t][i] * a[i][j]} * b[t][j]
-		}
-	}
+            alpha->table[t+1][j] = accum * hmm->observation[observ->seq[t+1]][j]; // alpha[t][j] = \sum{alpha[t][i] * a[i][j]} * b[t][j]
+        }
+    }
 
-	// Termination
-	double prob = 0;
-	for (i = 0; i < hmm->state_num; i++) {
-		prob += alpha->table[observ->seq_num-1][i];
-	}
+    // Termination
+    double prob = 0;
+    for (i = 0; i < hmm->state_num; i++) {
+        prob += alpha->table[observ->seq_num-1][i];
+    }
 
-	return;
+    return;
 }
 
 /**
@@ -49,29 +49,29 @@ void forward_algo(HMM *hmm, Observation *observ, Table *alpha)
  */
 void backward_algo(HMM *hmm, Observation *observ, Table *beta)
 {
-	int i, j, t;
-	beta->seq_num = observ->seq_num;
-	beta->state_num = hmm->state_num;
+    int i, j, t;
+    beta->seq_num = observ->seq_num;
+    beta->state_num = hmm->state_num;
 
-	// Initialization
-	for (i = 0; i < hmm->state_num; i++) {
-		beta->table[observ->seq_num-1][i] = 1; // beta[T][i] = 1
-	}
+    // Initialization
+    for (i = 0; i < hmm->state_num; i++) {
+        beta->table[observ->seq_num-1][i] = 1; // beta[T][i] = 1
+    }
 
-	// Induction
-	for (t = observ->seq_num - 2; t >= 0; t--) {
-		for (i = 0; i < hmm->state_num; i++) {
-			double accum = 0;
+    // Induction
+    for (t = observ->seq_num - 2; t >= 0; t--) {
+        for (i = 0; i < hmm->state_num; i++) {
+            double accum = 0;
 
-			for (j = 0; j < hmm->state_num; j++) {
-				accum += hmm->transition[i][j] * hmm->observation[observ->seq[t+1]][j] * beta->table[t+1][j];
-			}
+            for (j = 0; j < hmm->state_num; j++) {
+                accum += hmm->transition[i][j] * hmm->observation[observ->seq[t+1]][j] * beta->table[t+1][j];
+            }
 
-			beta->table[t][i] = accum; // beta[t][i] = \sum{a[i][j] * b[t+1][j] * beta[t+1][j]}
-		}
-	}
+            beta->table[t][i] = accum; // beta[t][i] = \sum{a[i][j] * b[t+1][j] * beta[t+1][j]}
+        }
+    }
 
-	return;
+    return;
 }
 
 /**
@@ -82,104 +82,104 @@ void backward_algo(HMM *hmm, Observation *observ, Table *beta)
  */
 void calc_delta(Table *alpha, Table *beta, Table *delta)
 {
-	int i, t;
-	double sum;
-	delta->seq_num = alpha->seq_num;
-	delta->state_num = alpha->state_num;
+    int i, t;
+    double sum;
+    delta->seq_num = alpha->seq_num;
+    delta->state_num = alpha->state_num;
 
-	for (t = 0; t < delta->seq_num; t++) {
-		sum = 0;
-		for (i = 0; i < delta->state_num; i++) {
-			delta->table[t][i] = alpha->table[t][i] * beta->table[t][i];
-			sum += delta->table[t][i];
-		}
+    for (t = 0; t < delta->seq_num; t++) {
+        sum = 0;
+        for (i = 0; i < delta->state_num; i++) {
+            delta->table[t][i] = alpha->table[t][i] * beta->table[t][i];
+            sum += delta->table[t][i];
+        }
 
-		// Normalization
-		for (i = 0; i < delta->state_num; i++) {
-			delta->table[t][i] /= sum;
-		}
-	}
+        // Normalization
+        for (i = 0; i < delta->state_num; i++) {
+            delta->table[t][i] /= sum;
+        }
+    }
 
-	return;
+    return;
 }
 
 void baum_welch_algo(HMM *hmm, Observation *observ, Table *alpha, Table *beta, Epsilon *epsilon)
 {
-	int i, j, t;
-	double sum;
-	epsilon->seq_num = alpha->seq_num;
-	epsilon->state_num = alpha->state_num;
+    int i, j, t;
+    double sum;
+    epsilon->seq_num = alpha->seq_num;
+    epsilon->state_num = alpha->state_num;
 
-	for (t = 0; t < epsilon->seq_num - 1; t++) {
-		sum = 0;
-		for (i = 0; i < epsilon->state_num; i++) {
-			for (j = 0; j < epsilon->state_num; j++) {
-				epsilon->table[t][i][j] = alpha->table[t][i] * \
-					hmm->transition[i][j] * \
-					hmm->observation[observ->seq[t+1]][j] * \
-					beta->table[t+1][j];
-				
-				sum += epsilon->table[t][i][j];
-			}
+    for (t = 0; t < epsilon->seq_num - 1; t++) {
+        sum = 0;
+        for (i = 0; i < epsilon->state_num; i++) {
+            for (j = 0; j < epsilon->state_num; j++) {
+                epsilon->table[t][i][j] = alpha->table[t][i] * \
+                    hmm->transition[i][j] * \
+                    hmm->observation[observ->seq[t+1]][j] * \
+                    beta->table[t+1][j];
+                
+                sum += epsilon->table[t][i][j];
+            }
 
-		}
+        }
 
-		// Normalization
-		for (i = 0; i < epsilon->state_num; i++) {
-			for (j = 0; j < epsilon->state_num; j++) {
-				epsilon->table[t][i][j] /= sum;
-			}
-		}
-	}
+        // Normalization
+        for (i = 0; i < epsilon->state_num; i++) {
+            for (j = 0; j < epsilon->state_num; j++) {
+                epsilon->table[t][i][j] /= sum;
+            }
+        }
+    }
 }
 
 void train_model(HMM *hmm, Table *delta, Epsilon *epsilon, Observation *train, int train_num)
 {
-	int i, j, k, t, n;
-	double sum_delta, sum_epsilon;
-	double numerator, denominator;
+    int i, j, k, t, n;
+    double sum_delta, sum_epsilon;
+    double numerator, denominator;
 
-	// update initial pi[i]
-	for (i = 0; i < hmm->state_num; i++) {
-		sum_delta = 0;
-		for (n = 0; n < train_num; n++) {
-			sum_delta += delta[n].table[0][i];
-		}
-		sum_delta /= train_num;
-		hmm->initial[i] = sum_delta;
-	}
+    // update initial pi[i]
+    for (i = 0; i < hmm->state_num; i++) {
+        sum_delta = 0;
+        for (n = 0; n < train_num; n++) {
+            sum_delta += delta[n].table[0][i];
+        }
+        sum_delta /= train_num;
+        hmm->initial[i] = sum_delta;
+    }
 
-	// update transition a[i][j]
-	for (i = 0; i < hmm->state_num; i++) {
-		for (j = 0; j < hmm->state_num; j++) {
-			sum_delta = 0;
-			sum_epsilon = 0;
-			for (n = 0; n < train_num; n++) {
-				for (t = 0; t < delta[n].seq_num - 1; t++) {
-					sum_epsilon += epsilon[n].table[t][i][j];
-					sum_delta += delta[n].table[t][i];
-				}
-			}
-			hmm->transition[i][j] = sum_epsilon / sum_delta;
-		}
-	}
+    // update transition a[i][j]
+    for (i = 0; i < hmm->state_num; i++) {
+        for (j = 0; j < hmm->state_num; j++) {
+            sum_delta = 0;
+            sum_epsilon = 0;
+            for (n = 0; n < train_num; n++) {
+                for (t = 0; t < delta[n].seq_num - 1; t++) {
+                    sum_epsilon += epsilon[n].table[t][i][j];
+                    sum_delta += delta[n].table[t][i];
+                }
+            }
+            hmm->transition[i][j] = sum_epsilon / sum_delta;
+        }
+    }
 
-	// update observation b[k][j]
-	for (k = 0; k < hmm->observ_num; k++) {
-		for (j = 0; j < hmm->state_num; j++) {
-			denominator = 0;
-			numerator = 0;
-			for (n = 0; n < train_num; n++) {
-				for (t = 0; t < delta[n].seq_num; t++) {
-					if (train[n].seq[t] == k) {
-						numerator += delta[n].table[t][j];
-					}
-					denominator += delta[n].table[t][j];
-				}
-			}
-			hmm->observation[k][j] = numerator / denominator;
-		}
-	}
+    // update observation b[k][j]
+    for (k = 0; k < hmm->observ_num; k++) {
+        for (j = 0; j < hmm->state_num; j++) {
+            denominator = 0;
+            numerator = 0;
+            for (n = 0; n < train_num; n++) {
+                for (t = 0; t < delta[n].seq_num; t++) {
+                    if (train[n].seq[t] == k) {
+                        numerator += delta[n].table[t][j];
+                    }
+                    denominator += delta[n].table[t][j];
+                }
+            }
+            hmm->observation[k][j] = numerator / denominator;
+        }
+    }
 }
 
 Observation train[MAX_TRAIN_LINE];
@@ -188,42 +188,42 @@ Epsilon epsilon[MAX_TRAIN_LINE];
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4 + 1) {
-		printf("Wrong argument format\n");
-		printf("Usage: ./train iteration model_init.txt seq_model_0X.txt model_0X.txt\n");
-		exit(1);
-	}
+    if (argc != 4 + 1) {
+        printf("Wrong argument format\n");
+        printf("Usage: ./train iteration model_init.txt seq_model_0X.txt model_0X.txt\n");
+        exit(1);
+    }
 
-	int i, n, train_num;
-	char *ptr;
+    int i, n, train_num;
+    char *ptr;
 
-	const int iter = strtol(argv[1], &ptr, 10);
-	const char *model_init = argv[2];
-	const char *train_file = argv[3];
-	const char *model_file = argv[4];
+    const int iter = strtol(argv[1], &ptr, 10);
+    const char *model_init = argv[2];
+    const char *train_file = argv[3];
+    const char *model_file = argv[4];
 
-	HMM hmm_initial;
-	loadHMM(&hmm_initial, model_init);
-	dumpHMM(stderr, &hmm_initial);
+    HMM hmm_initial;
+    loadHMM(&hmm_initial, model_init);
+    dumpHMM(stderr, &hmm_initial);
 
-	train_num = get_data(train, train_file);
+    train_num = get_data(train, train_file);
 
-	for (i = 0; i < iter; i++) {
-		printf("\n##### iteration: %d #####\n", i + 1);
-		for (n = 0; n < train_num; n++) {
-			forward_algo(&hmm_initial, &train[n], &alpha[n]);
-			backward_algo(&hmm_initial, &train[n], &beta[n]);
-			calc_delta(&alpha[n], &beta[n], &delta[n]);
-			baum_welch_algo(&hmm_initial, &train[n], &alpha[n], &beta[n], &epsilon[n]);
-		}
-		train_model(&hmm_initial, delta, epsilon, train, train_num);
-		dumpHMM(stderr, &hmm_initial);
-	}
+    for (i = 0; i < iter; i++) {
+        printf("\n##### iteration: %d #####\n", i + 1);
+        for (n = 0; n < train_num; n++) {
+            forward_algo(&hmm_initial, &train[n], &alpha[n]);
+            backward_algo(&hmm_initial, &train[n], &beta[n]);
+            calc_delta(&alpha[n], &beta[n], &delta[n]);
+            baum_welch_algo(&hmm_initial, &train[n], &alpha[n], &beta[n], &epsilon[n]);
+        }
+        train_model(&hmm_initial, delta, epsilon, train, train_num);
+        dumpHMM(stderr, &hmm_initial);
+    }
 
-	printf("Dump HMM model to file: %s\n", model_file);
-	FILE *fp = open_or_die(model_file, "w");
-	dumpHMM(fp, &hmm_initial);
-	fclose(fp);
+    printf("Dump HMM model to file: %s\n", model_file);
+    FILE *fp = open_or_die(model_file, "w");
+    dumpHMM(fp, &hmm_initial);
+    fclose(fp);
 
-	return 0;
+    return 0;
 }
